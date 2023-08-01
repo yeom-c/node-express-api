aeee488b1e4a69570fddd3f402b5e823032efd71
matisco-yeomc <yeomc@imantisco.com>
2023-08-01 14:56:43 +0900

    feat: HTTP request logger middleware morgan

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ const express = require('express');
 const httpStatus = require('http-status');
 const config = require('./config/config');
 const logger = require('./config/logger');
+const morgan = require('./config/morgan');
 const { errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/api-error');
 
 async function bootstrap() {
   const app = express();
   const port = config.port;
-  
+
+  if (config.env !== 'test') {
+    app.use(morgan.successHandler);
+    app.use(morgan.errorHandler);
+  }
+
   // 잘못된 api 접근시 에러처리
   app.use((req, res, next) => {
     next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
